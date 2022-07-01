b116d3cfa90a9826918f7c86141c360712b04869
Ayikanying <allanayikanying72@gmail.com>
2022-07-01 13:04:51 -0700

    Task 3 alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/** 
+ * main - print alphabet in lower and upper
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int ch;
+	int CH;
+
+	for (cha = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (CH = 'A'; CH <= 'Z'; CH++)
+		putchar(CH);
+	putchar('\n');
+	return (0);
+}
