db9805818f439d7d7833e9fe4bb24b8efaba199c
Ayikanying <allanayikanying72@gmail.com>
2022-07-05 16:14:21 -0700

    program for printing tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,7 +31,7 @@ void times_table(void)
 			else
 			{
 				_putchar((n / 10) + '0');
-				_putchar((n % 100) + '0');
+				_putchar((n % 10) + '0');
 				if (j == 9)
 					continue;
 				_putchar(',');
