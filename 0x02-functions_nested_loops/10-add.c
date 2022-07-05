eda9ef62050195318fc8ae253358b84ddfe9ff17
Ayikanying <allanayikanying72@gmail.com>
2022-07-05 16:19:45 -0700

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integrers and returns the result
+ *
+ * @a: number being added
+ *
+ * @b: number being added
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
