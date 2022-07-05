0d679c867e3fd31dc1ceae66fb7b610ccfe55166
Ayikanying <allanayikanying72@gmail.com>
2022-07-05 12:49:31 -0700

    used _putchar to print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * main - prints _putchar
@@ -8,6 +8,13 @@
  */
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
