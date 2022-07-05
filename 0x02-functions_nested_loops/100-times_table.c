a6f208863bd8bc8babe1291e5dbffd5eb60dfb0e
Ayikanying <allanayikanying72@gmail.com>
2022-07-05 16:53:24 -0700

    prints tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,8 +41,8 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar(((k /10) % 10) + '0');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}
