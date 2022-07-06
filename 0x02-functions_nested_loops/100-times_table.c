e2e16603767d63061cd05e0d6db5fa979de5b949
Ayikanying <allanayikanying72@gmail.com>
2022-07-05 17:49:36 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,7 +27,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 1i0 + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
