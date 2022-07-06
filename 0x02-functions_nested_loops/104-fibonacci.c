81a5d625db113b16c1cac1f417f420e6e19e36b8
Ayikanying <allanayikanying72@gmail.com>
2022-07-05 18:42:52 -0700

    fibonacci no hardcode

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,41 +5,39 @@
  *
  * Purpose - no hardcode
  *
- * Return: (Success)
+ * Return:  (Success)
  */
 int main(void)
 {
 	unsigned long int i;
-	unsigned long int a = 1;
-	unsigned long int b = 2;
-	unsigned long int c = 1000000000;
-	unsigned long int d;
-	unsigned long int e;
-	unsigned long int f;
-	unsigned long int g;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	printf("%lu", a);
+	printf("%lu", bef);
 	for (i = 1; i < 91; i++)
 	{
-		printf(", %lu", b);
-		b += a;
-		a = b - a;
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
-	d = (a / 1);
-	e = (a % 1);
-	f = (b / 1);
-	g = (b % 1);
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", f + (g / c));
-		printf("%lu", g % 1);
-		f = f + d;
-		d = f - d;
-		g = g + e;
-		e = g - e;
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
 }
-
-	
