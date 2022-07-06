a22b262775b2c050a333be1a1d4197e8b14f7398
Ayikanying <allanayikanying72@gmail.com>
2022-07-05 17:47:45 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "main.h"
+
 /*
  * print_times_table - prints the n times table, starting with 0
  *
- * @n: number of the times table
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
@@ -26,7 +27,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar(((prod / 10)) % 1i0 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
