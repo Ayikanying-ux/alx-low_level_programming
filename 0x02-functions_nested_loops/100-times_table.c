546914acb583b20ef33be30ebca64d0ee8beadca
Ayikanying <allanayikanying72@gmail.com>
2022-07-05 17:52:02 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
 /*
  * print_times_table - prints the n times table, starting with 0
  *
- * @n: The value of the times table to be printed.
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
