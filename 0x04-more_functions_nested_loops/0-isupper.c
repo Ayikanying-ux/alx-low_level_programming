ba63f7ad8e7eae10957f0baab7a9eb95eb4b8d00
Ayikanying <allanayikanying72@gmail.com>
2022-07-07 02:44:02 -0700

    checking a number is capital

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,15 @@
-	
+#include "main.h"
+
+/**
+ * _isupper - checks if input is an uppercase letter
+ *
+ * @c: integer to check
+ *
+ * Return: 1 if true, 0 if false
+ */
+int _isupper(int c)
+{
+	if (c >= "A" && c <= "Z")
+		return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char);
+int _isupper(int);
+
+#endif
