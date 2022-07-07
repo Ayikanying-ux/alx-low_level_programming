940a91e08d9e8e7a8f41d9daf9c5f4333bf040dd
Ayikanying <allanayikanying72@gmail.com>
2022-07-07 03:48:38 -0700

    printing line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - print line of variable length
+ *
+ * @n: length of line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	_putchar('\n');
+}
