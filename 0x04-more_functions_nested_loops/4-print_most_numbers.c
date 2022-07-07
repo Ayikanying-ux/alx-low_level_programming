3b3c51f1ecd9e0d1ff170d0cf4e2ee1820fbed7f
Ayikanying <allanayikanying72@gmail.com>
2022-07-07 03:23:29 -0700

    Task 4 I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i == 2 || i == 4)
-		{
+		if (i == 50 ||  i == 52)
 			continue;
-		}
-		_putchar(i);
+		else
+			_putchar(i);
 	}
 	_putchar('\n');
 }
