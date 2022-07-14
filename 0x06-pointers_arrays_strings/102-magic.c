caa5e02b5462e84586e787d195ae115176ed321d
Ayikanying <allanayikanying72@gmail.com>
2022-07-14 02:38:35 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,10 +10,15 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
+	 *
 	 * Remember:
+	 *
 	 * - you are not allowed to use a
+	 *
 	 * - you are not allowed to modify p
+	 *
 	 * - only one statement
+	 *
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
