f337cf16cf2aa740b3e0ed401bf7e077a372295e
Ayikanying <allanayikanying72@gmail.com>
2022-07-14 02:13:20 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,11 +8,11 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
+	/*
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
-	 * you are not allowed to modify p
+	 * - you are not allowed to modify p
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
