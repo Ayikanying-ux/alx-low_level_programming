f69c17d14dd923df5f7bd7a0f98b43d5f2d512f5
Ayikanying <allanayikanying72@gmail.com>
2022-07-18 09:40:08 -0700

    Hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,11 +3,8 @@
 
 /**
  * _strchr - Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
- *
  * @s: string to be searched
- *
  * @c: target char
- *
  * Return: pointer to first occurrence of c or NULL if char not found
  */
 char *_strchr(char *s, char c)
