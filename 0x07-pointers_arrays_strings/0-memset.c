aef0815e244dce7b266adfdbdf5eef53eb4a3aa8
Ayikanying <allanayikanying72@gmail.com>
2022-07-18 02:48:43 -0700

    Initial commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h""
+
+/**
+ * *_memset - fills memory with a constant byte
+ *
+ * @s: pointer to be filled
+ *
+ * @b: constant to be filled in memory
+ *
+ * @n: number of times the constant is filed in memory
+ *
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+_putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+#endif
