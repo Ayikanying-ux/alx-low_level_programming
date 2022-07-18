4de97a8cbda239ef7b86070eb1afe9203bbfd2f8
Ayikanying <allanayikanying72@gmail.com>
2022-07-18 10:08:28 -0700

    Task 3 strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,18 +11,13 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int counter, flag, i;
+	int counter, i;
 	
 	counter = 0;
 	for (i = 0; i != '\0'; i++)
 	{
 		if (*(s + i) == *(accept + i))
-		{
 			counter++;
-			flag = 1;
-		}
-		if (flag == 0)
-			return (counter);
 	}
-	return (0);
+	return (counter);
 }
