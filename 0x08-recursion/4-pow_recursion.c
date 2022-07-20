7f3eab636850a3f5dea3c2cd8d1ba908accc2a2a
Ayikanying <allanayikanying72@gmail.com>
2022-07-20 06:14:04 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,8 +10,10 @@ int _pow_recursion(int x, int y)
 {
 	if (y >= 0)
 	{
-		if (y < 0)
-			return (-1);
+		if (y == 0)
+			return (1);
 		return (x * _pow_recursion(x, y - 1));
 	}
+	else
+		return (-1);
 }
