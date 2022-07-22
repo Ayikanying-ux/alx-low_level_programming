b35f66210017c98051c84c559e2577f82c705feb
Ayikanying <allanayikanying72@gmail.com>
2022-07-22 09:31:49 -0700

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line
+ * @argc - argument count
+ * @argv - argument vector
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
