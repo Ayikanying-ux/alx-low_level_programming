de1964af260a88910d3529c20294f65251d998bf
Ayikanying <allanayikanying72@gmail.com>
2022-07-22 10:07:45 -0700

    Hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 
 /**
  * main - prints its name followed by a new line
- * @argc - argument count
- * @argv - argument vector
+ * @argc: argument count
+ * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, char* argv)
+{
+	(void)argv;
+	printf("%d", argc
