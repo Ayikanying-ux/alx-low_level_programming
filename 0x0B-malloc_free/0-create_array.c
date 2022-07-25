45cf22467a321c16afc949217e41aceab33359e2
Ayikanying <allanayikanying72@gmail.com>
2022-07-25 07:28:04 -0700

    Initial commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,8 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *ptr;
-
+	
+	ptr = malloc(size * sizeof(char));
 	if (size == 0 || !ptr)
 		return ('\0');
 	for (i = 0; i < size; i++)
