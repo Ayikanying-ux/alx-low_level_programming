d6950a6771059c3ac1ca266fb61799452fbadfdf
Ayikanying <allanayikanying72@gmail.com>
2022-07-25 13:17:07 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _str_length - prints the length
+ * @str: pointer to get length
+ * Return: length of string
+ */
+int _str_length(char *str)
+{
+	int length = 0;
+
+	while (*str)
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to be copied
+ * Return: copied string
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	char *_copy;
+	
+	if (!str)
+		return (NULL);
+	copy = malloc(_str_length(str) * sizeof(char));
+	if (!copy)
+		return (NULL);
+	_copy = copy;
+	while (*str)
+	{
+		*_copy = *str;
+		_copy++;
+		str++;
+	}
+	*_copy = '\0';
+	return (copy);
+}
+	
