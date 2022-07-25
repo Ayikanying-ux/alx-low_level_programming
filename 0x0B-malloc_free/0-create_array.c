85a09c3e62639595dc30a70a7c1607e7d8060f56
Ayikanying <allanayikanying72@gmail.com>
2022-07-25 07:24:45 -0700

    Initial commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char
+ * @size: size of array
+ * @c: char to fill array with
+ * Return: string of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (size == 0 || !ptr)
+		return ('\0');
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
+}
