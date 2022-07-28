2c7885396f5a782670636e9bac8d82fcbf03812e
Ayikanying <79021301+Ayikanying-ux@users.noreply.github.com>
2022-07-28 23:13:15 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,13 +11,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-void *_calloc(unsigned int bytes, unsigned int size);
-void print_me(int *sum_result, int len_r);
-int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r);
-int is_digit(char c);
-void add_arrays(int *mul_result, int *sum_result, int len_r);
-int str_len(char *str);
-void *malloc_checked(unsigned int b);
-int *array_range(int min, int max);
-void print_int(unsigned long int n);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
 #endif
