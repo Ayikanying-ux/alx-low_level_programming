0a0c2f8e0eaaed9d58b67c9667d25532481bfaf4
Ayikanying <allanayikanying72@gmail.com>
2022-07-28 05:06:07 -0700

    added a short description to the _str_len function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size = len1 + n;
 	ptr = malloc(sizeof(char) * size + 1);
 	if (!ptr)
-		return ('\0');
+		return (NULL);
 	for (i = 0; i < len1; i++)
 		ptr[i] = s1[i];
 	for (j = 0; j < n; j++, i++)
