fcd8005f30ad4bfbfb380610de9db82b91cd21aa
Ayikanying <allanayikanying72@gmail.com>
2022-08-01 12:43:26 -0700

    created a fuction that create a new variable of type dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new variable of type struct dog
+ * @name: name of the dog
+ * @float: age of the dog
+ * @owner: owner of the dog
+ * Return: A variable of type dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+}
