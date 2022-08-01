9bd58373d687b4f4dd89901135a4b7562025e82b
Ayikanying <allanayikanying72@gmail.com>
2022-08-01 11:30:58 -0700

    created dog.h file that defines a new structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creates a new structure of dog
+ * @name: struct member name
+ * @owner: struct member owner
+ * @age: struct member age
+ *
+ * Description: Creates a structure of name dog and its characteristics
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif
