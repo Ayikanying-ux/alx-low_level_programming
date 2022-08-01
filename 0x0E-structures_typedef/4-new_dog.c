2b92b5ea2b9aa8b9e4e8d0da6150146932276de2
Ayikanying <allanayikanying72@gmail.com>
2022-08-01 13:21:04 -0700

    removed a copy of the age from the newly allocated memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,9 +12,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *p;
 
 	p = malloc(sizeof(dog_t));
-	if (!p)
+	if (p == NULL)
 		return (NULL);
 	p->name = name;
+	p->age = age;
 	p->owner = owner;
 	return (p);
 }
