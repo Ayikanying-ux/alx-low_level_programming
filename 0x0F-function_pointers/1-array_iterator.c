e49062fb627a0a18dab6d342e8ad11b5f8f0c994
Ayikanying <allanayikanying72@gmail.com>
2022-08-03 05:14:14 -0700

    reduced the function description

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - executes a func
  * @array: array passed to the function
  * @size: size of array
  * @action: pointer to function
