305fa0bcd34a8544d7f3200b10ec7873e4305bab
Ayikanying <allanayikanying72@gmail.com>
2022-08-04 03:18:56 -0700

    removed trailing space on line 15

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (separator)
+		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
