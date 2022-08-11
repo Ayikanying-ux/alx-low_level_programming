a9d440f987be23d47d7d661228180f6323b1da3e
Ayikanying <allanayikanying72@gmail.com>
2022-08-11 13:16:23 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node-> = strdup(str);
+	new_node->str = strdup(str);
 	for (n = 0; str[n]; n++)
 		;
 	new_node->len = n;
