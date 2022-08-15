d0f54fd24feff4fba832300516abbc7336e80939
Ayikanying <allanayikanying72@gmail.com>
2022-08-15 12:22:41 -0700

    removed trailing spaces

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,10 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t number_of_nodes = 0;
-	int i;
-	
+
 	while (h != NULL)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		number_of_nodes++;
 		h = h->next;
 	}
