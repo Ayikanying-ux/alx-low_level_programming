ebc82543b2ac330da6e2026f5e6d079bae013782
Ayikanying <allanayikanying72@gmail.com>
2022-08-16 06:15:55 -0700

    Task 9 insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *new_node;
 	int i = 0;
-	
+
 	temp = *head;
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (!new_node)
