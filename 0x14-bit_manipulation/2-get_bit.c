e2e4a1ac19cfa35b25849788f9ad0ede09ed6475
Ayikanying <allanayikanying72@gmail.com>
2022-08-18 06:13:43 -0700

    Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *get_bit-function that returns the value of a bit at a given index
+ *@n:number
+ *@index: the number of times to be shifted
+ *Return: nth bit - bitstatus
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bitstatus;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	bitstatus = (n >> index) & 1;/*nth bit of n right shift n, index times*/
+	return (bitstatus);
+}
